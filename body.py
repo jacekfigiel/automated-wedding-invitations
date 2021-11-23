597537ca9b4a59deec13117e6f0b8b13e01e76f3
jacekfigiel <jacekfigo1988@gmail.com>
2021-11-23 06:30:24 +0000

    For loop to be fixed

--- body.py
+++ body.py
@@ -1,7 +1,17 @@
 from docx import Document
 
 doc = Document("Invitation.docx")
+inv_docx = Document("List of guest.docx")
 text = doc.paragraphs[1]
 
-prior_paragraph = text.insert_paragraph_before(text='Jan Kowalski', style=None)
-doc.save("Invitation.docx")
+all_guests = []
+
+all_paragraphs = inv_docx.paragraphs
+for para in all_paragraphs:
+    all_guests.append(para.text)
+
+for g in all_guests:
+    prior_paragraph = text.insert_paragraph_before(text=g,
+                                                   style="Normal")
+    file_name = "Invitation" + g
+    doc.save("Invitation for" + g + ".docx")
